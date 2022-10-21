f6350b21d80b7410dfa521a3983f3cdc4ef290c1
eri <eri@spyro-soft.com>
2022-10-21 14:24:30 +0200

    Updates InitWindow

--- Demos/SpriteTest/Program.cs
+++ Demos/SpriteTest/Program.cs
@@ -9,7 +9,7 @@ public static class Program
     public static int Main()
     {
         SetConfigFlags((uint)(ConfigFlags.FLAG_WINDOW_RESIZABLE | ConfigFlags.FLAG_MSAA_4X_HINT));
-		InitWindow(GetScreenWidth(), GetScreenHeight(), Title);
+		InitWindow(0, 0, Title);
 		
         SetTargetFPS(144);
         var bunnyScene = new SpriteScene(Title);
